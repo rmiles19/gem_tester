07235e3a52f7e1bf8a5c988e536cb5c32a48059d
Rachel Miles <milesrachel9@gmail.com>
2018-04-18 11:10:57 -0600

    testing

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,4 +1,7 @@
 class ExampleController < ApplicationController
   def index
+    @reversed = StringHelperRmiles19.reversify('This is backwards')
+    @cased = StringHelperRmiles19.casify('this is a craaaazy sentence')
+    @spaced = StringHelperRmiles19.spacify('holla', 3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'example/index'
+  root 'example#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
